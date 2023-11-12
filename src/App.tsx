87655650995cbf13711e4c6dde9d422c5baf34cb
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2023-11-12 13:52:42 -0300

    ✨ feat: initial template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl font-bold">
+        Initial template with Tailwind and Shadcn/ui
+      </h2>
+    </>
+  );
+}
+
+export default App;
