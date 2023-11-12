2bd069717b27cd22ef189d440763ce8a010172de
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2023-11-12 15:04:24 -0300

    ✨ feat: add upload files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import UploadComponent from "./components/UploadDragDeop";
+
 function App() {
   return (
     <>
-      <h2 className="text-3xl font-bold">
-        Initial template with Tailwind and Shadcn/ui
-      </h2>
+      <div className="w-screen bg-gray-300 h-screen flex flex-col justify-center items-center">
+        <h2 className="text-3xl font-bold mb-5">
+          Implementation with Vite, Tailwind and Shadcn/ui
+        </h2>
+        <div>
+          <UploadComponent />
+        </div>
+      </div>
     </>
   );
 }
